d9bc212e19ef511fe4a208a5c73a66813515d769
LEE-sh1673 <lsh901673@gmail.com>
2023-07-10 12:24:27 +0900

    refactor: remove message

--- src/main/java/com/devlog/DevlogApplication.java
+++ src/main/java/com/devlog/DevlogApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevlogApplication {
 
     public static void main(String[] args) {
-        System.out.println("DevlogApplication.test");
         SpringApplication.run(DevlogApplication.class, args);
     }
 
